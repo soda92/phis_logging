7a9580d546a1ad97eb2a8b483d097350e58c50b4
Maple <wjxa20152015@gmail.com>
2025-07-21 15:21:32 +0800

    add auto make result dir

--- phis_logging/dir_switch.py
+++ phis_logging/dir_switch.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 import os
 
-def setup_dir():
+def switch_dir():
     cwd = Path.cwd()
     if cwd.joinpath('文档').exists():
         return
     elif cwd.parent.joinpath('文档').exists():
         os.chdir(cwd.parent)
+
+
+def setup_dir():
+    switch_dir()
+
+    if not Path('执行结果').exists():
+        Path('执行结果').mkdir(parents=True, exist_ok=True)
